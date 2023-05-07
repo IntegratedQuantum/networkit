4bcb48cc827ae9edbd8585a5ea6948fb5e052b5c
IntegratedQuantum <jahe788@gmail.com>
2023-05-07 18:23:20 +0200

    Implement the Rmat algorithm from Hübschle-Schneider and Sanders.

--- networkit/cpp/generators/RmatGenerator.cpp
+++ networkit/cpp/generators/RmatGenerator.cpp
@@ -3,6 +3,9 @@
  *
  *  Created on: 18.03.2014
  *      Author: Henning, cls
+ *
+ * Uses the algorithm described by Hübschle-Schneider and Sanders in
+ * "Linear Work Generation of R-MAT Graphs" https://arxiv.org/abs/1905.03525
  */
 
 #include <networkit/auxiliary/Log.hpp>
@@ -25,6 +28,146 @@ RmatGenerator::RmatGenerator(count scale, count edgeFactor, double a, double b, 
     defaultEdgeWeight = 1.0;
 }
 
+struct Entry {
+    uint32_t i;
+    uint32_t j;
+    uint32_t numberOfBits;
+    double probability;
+};
+
+struct AliasTable {
+    std::vector<std::pair<uint32_t, uint32_t>> bits;
+    std::vector<uint8_t> numberOfBits;
+    std::vector<uint32_t> coinFlipProbability;
+    std::vector<uint32_t> coinFlipReplacement;
+    uint32_t mask;
+    std::pair<uint32_t, uint32_t> curBits{0, 0};
+    uint32_t remainingBits = 0;
+
+    void sample(std::mt19937_64 &urng) {
+        std::uniform_int_distribution<uint64_t> dist;
+        uint64_t randomNumber = dist(urng);
+        uint32_t index = randomNumber & mask;
+        uint32_t coinFlip = randomNumber >> 32;
+        if (coinFlip <= coinFlipProbability[index]) {
+            index = coinFlipReplacement[index];
+        }
+        curBits = bits[index];
+        remainingBits = numberOfBits[index];
+    }
+
+    std::pair<node, node> sampleEdge(std::mt19937_64 &urng, uint8_t bits) {
+        std::pair<node, node> result{0, 0};
+        while (true) {
+            if (remainingBits >= bits) {
+                remainingBits -= bits;
+                result.first = result.first << bits | curBits.first >> remainingBits;
+                result.second = result.second << bits | curBits.second >> remainingBits;
+                curBits.first &= (1 << remainingBits) - 1;
+                curBits.second &= (1 << remainingBits) - 1;
+                return result;
+            }
+            result.first = result.first << remainingBits | curBits.first;
+            result.second = result.second << remainingBits | curBits.second;
+            bits -= remainingBits;
+
+            sample(urng);
+        }
+    }
+};
+
+bool priorityQueueCompare(const Entry &a, const Entry &b) {
+    return a.probability < b.probability && (a.numberOfBits < 32 || b.numberOfBits == 32);
+}
+
+// std::priority_queue doesn't support iterating over its elements.
+struct PriorityQueue {
+    std::vector<Entry> &internal;
+
+    PriorityQueue(std::vector<Entry> &vec) : internal(vec) {}
+
+    void push(Entry &&entry) {
+        internal.push_back(entry);
+        std::push_heap(internal.begin(), internal.end(), priorityQueueCompare);
+    }
+
+    Entry pop() {
+        std::pop_heap(internal.begin(), internal.end(), priorityQueueCompare);
+        Entry result = internal.back();
+        internal.pop_back();
+        return result;
+    }
+};
+
+void generateEntries(std::vector<Entry> &entryList, count size, double a, double b, double c,
+                     double d) {
+    entryList.reserve(size);
+    PriorityQueue priorityQueue(entryList);
+    priorityQueue.push(Entry{0, 0, 1, a});
+    priorityQueue.push(Entry{0, 1, 1, b});
+    priorityQueue.push(Entry{1, 0, 1, c});
+    priorityQueue.push(Entry{1, 1, 1, d});
+    while (entryList.size() <= size - 3) {
+        // Take the entry with the highest probability and split it up.
+        // This increases the average number of bits compared to a uniform distribution.
+        Entry old = priorityQueue.pop();
+        priorityQueue.push(
+            Entry{old.i << 1 | 0, old.j << 1 | 0, old.numberOfBits + 1, a * old.probability});
+        priorityQueue.push(
+            Entry{old.i << 1 | 0, old.j << 1 | 1, old.numberOfBits + 1, b * old.probability});
+        priorityQueue.push(
+            Entry{old.i << 1 | 1, old.j << 1 | 0, old.numberOfBits + 1, c * old.probability});
+        priorityQueue.push(
+            Entry{old.i << 1 | 1, old.j << 1 | 1, old.numberOfBits + 1, d * old.probability});
+    }
+}
+
+void generateTable(AliasTable &table, count size, double a, double b, double c, double d) {
+    assert(size & size - 1 == 0); // The size must be a power of two.
+    std::vector<Entry> entryList;
+    generateEntries(entryList, size, a, b, c, d);
+    while (entryList.size() < size) { // Fill the remaining entries with 0-probability entries.
+        entryList.push_back(Entry{0, 0, 0, 0});
+    }
+
+    // Construct the alias table:
+    table.mask = size - 1;
+    table.bits.resize(size);
+    table.numberOfBits.resize(size);
+    table.coinFlipProbability.resize(size);
+    table.coinFlipReplacement.resize(size);
+    for (int i = 0; i < size; i++) {
+        table.bits[i] = std::make_pair(entryList[i].i, entryList[i].j);
+        table.numberOfBits[i] = entryList[i].numberOfBits;
+        table.coinFlipProbability[i] = 0;
+        table.coinFlipReplacement[i] = 0;
+    }
+    double baseProbability = 1.0 / (double)size;
+    int lastOverfullIndex = 0;
+    int lastUnderfullIndex = 0;
+    while (true) {
+        while (entryList[lastOverfullIndex].probability <= baseProbability) {
+            lastOverfullIndex++;
+            if (lastOverfullIndex == size)
+                return;
+        }
+        while (entryList[lastUnderfullIndex].probability >= baseProbability) {
+            lastUnderfullIndex++;
+            if (lastUnderfullIndex == size)
+                return;
+        }
+        double delta = baseProbability - entryList[lastUnderfullIndex].probability;
+        entryList[lastUnderfullIndex].probability = 1;
+        entryList[lastOverfullIndex].probability -= delta;
+        table.coinFlipReplacement[lastUnderfullIndex] = lastOverfullIndex;
+        table.coinFlipProbability[lastUnderfullIndex] =
+            (uint32_t)(delta / baseProbability * std::numeric_limits<uint32_t>::max());
+        if (entryList[lastOverfullIndex].probability < baseProbability) {
+            lastUnderfullIndex = std::min(lastUnderfullIndex, lastOverfullIndex);
+        }
+    }
+}
+
 Graph RmatGenerator::generate() {
     double n = (1 << scale);
     if (n <= reduceNodes) {
@@ -36,8 +179,10 @@ Graph RmatGenerator::generate() {
     Graph G(n - reduceNodes, weighted);
     double ab = a + b;
     double abc = ab + c;
+    AliasTable table;
+    generateTable(table, 64, a, b, c, 1 - a - b - c); // TODO: Dynamic table size.
 
-    auto quadrant([&]() {
+    auto quadrant([&]() { // TODO: Remove the old code.
         double r = Aux::Random::probability();
         TRACE("r: ", r);
 
@@ -51,8 +196,8 @@ Graph RmatGenerator::generate() {
             return 3;
     });
 
-    auto drawEdge([&]() {
-        node u = 0;
+    auto drawEdge([&]() { // TODO: Remove the old code.
+        /*node u = 0;
         node v = 0;
         for (index i = 0; i < scale; ++i) {
             count q = quadrant();
@@ -62,7 +207,8 @@ Graph RmatGenerator::generate() {
             v = v | (q & 1);
         }
 
-        return std::make_pair(u, v);
+        return std::make_pair(u, v);*/
+        return table.sampleEdge(Aux::Random::getURNG(), (uint8_t)scale);
     });
 
     if (reduceNodes > 0) {
